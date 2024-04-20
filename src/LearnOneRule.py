1becbcef83ef9ad3f0b38f9cc4a7fcfc87db59d4
Alex-wsl2 <abaekey@knights.ucf.edu>
2024-04-20 11:05:57 -0400

    Getting results for LearnOneRule, forming foundation for sequential coverage

--- src/LearnOneRule.py
+++ src/LearnOneRule.py
@@ -3,27 +3,39 @@ import inspect
 #https://www.geeksforgeeks.org/sequential-covering-algorithm/#
 
 def LearnOneRule(atoms, df):
+    print(df.head())
     for feature in atoms:
         #print(feature)
         #print(atoms[feature])
         for condition in atoms[feature]:
-            print(inspect.getsource(condition))
-            match = 0
-            nomatch = 0
-            #print(condition)
-            #TODO split up data into mispredicted/correctly predicted
-            for data in df[feature]:
-                #mask = (f(df) for f in atoms[feature])
-                testbool = condition(data)
-                if testbool == True:
-                    match +=1
-                elif testbool == False:
-                    nomatch +=1
             print(feature)
             print(condition)
-            print("matches: ", match)
-            print("nomatch: ", nomatch) 
-    
+            #print(inspect.getsource(condition))
+            mispred_match = 0
+            mispred_nomatch = 0
+            corpred_match = 0
+            corpred_nomatch = 0
+            #print(condition)
+            #TODO split up data into mispredicted/correctly predicted
+            for i, data in enumerate(df[feature]):
+                #mask = (f(df) for f in atoms[feature])
+                testbool = condition(data)
+                if (testbool == True) and (df.iloc[i]['mispredict']==1):
+                    mispred_match +=1
+                elif (testbool == False) and (df.iloc[i]['mispredict']==1):
+                    mispred_nomatch +=1
+                elif (testbool == True) and (df.iloc[i]['mispredict']==0):
+                    corpred_match +=1
+                elif (testbool == False) and (df.iloc[i]['mispredict']==0):
+                    corpred_nomatch +=1
+            print("mispred match: ", mispred_match)
+            print("mispred nomatch: ", mispred_nomatch)
+            print("corpred match: ", corpred_match)
+            print("corpred nomatch: ", corpred_nomatch)
+
+
+        
+
     #new_rule = atom
     #covered = # data that new rule includes
     #df = df-covered
